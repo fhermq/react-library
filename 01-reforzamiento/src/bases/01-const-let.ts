3df97f7d7fc708c53c2f63850b9370a404b6ae04
Fernando Mirasol <fhermq@gmail.com>
2025-09-15 17:22:31 -0600

    Se agrega repaso 01 Let Const

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,10 @@
+let firstName = 'Fernando';
+let lastName = 'Mirasol';
+
+let diceNumber = 5;
+diceNumber = 3;
+
+const containsLetterH = lastName.includes('l')
+
+console.log({ containsLetterH, diceNumber, firstName, lastName })
+
