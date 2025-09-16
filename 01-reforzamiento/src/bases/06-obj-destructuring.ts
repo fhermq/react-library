6de90b7981315033d466134d4ce7a2e4035a8158
Fernando Mirasol <fhermq@gmail.com>
2025-09-16 17:09:48 -0600

    Se agrega 06 Obj Destructuring de objetos

---
+++ 01-reforzamiento/src/bases/06-obj-destructuring.ts
@@ -0,0 +1,34 @@
+const person = {
+    name: 'Tony',
+    age: 40,
+    key: 'Ironman'
+}
+//Desestructuracion
+const { age: ironmanage, key, name: ironmanname } = person;
+
+console.log({ ironmanage, key, ironmanname })
+
+interface Hero {
+    name: string,
+    age: number,
+    key: string;
+    rank?: string;
+}
+
+const useContext = ({ name, age, key, rank ='sin rango' }: Hero) => {
+    return {
+        keyName: key,
+        user: {
+            name,
+            age
+        },
+        rank
+    };
+};
+
+const context = useContext(person);
+
+//
+const {rank, keyName, user:{name}, user:{age} } = useContext(person);
+
+console.log({rank, keyName, name, age});
