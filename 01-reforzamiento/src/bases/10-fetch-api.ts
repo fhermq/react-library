2841bf30ff96443ea05cf9a4e3a57d27907ec083
Fernando Mirasol <fhermq@gmail.com>
2025-09-22 11:13:05 -0600

    Se agregan fetchapi, tipado con interfaces y separacion de funciones

--- 01-reforzamiento/src/bases/10-fetch-api.ts
+++ 01-reforzamiento/src/bases/10-fetch-api.ts
@@ -1,16 +1,23 @@
+import type { GiphyRandomResponse } from "../data/giphy.response";
+
 const API_KEY = 'VZj9IQnKU8rN5cKqQnu29Fx3gX44dE4D';
 
 const myRequest = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`);
 
+const createImageInsideDOM = (url: string) => {
+    const imgElement = document.createElement('img');
+    imgElement.src = url;
+    document.body.append(imgElement);
+}
+
+
 myRequest
-.then((response) => response.json())
-.then((data)=> {
-    const imageURL = data.data.images.original.url;
-    console.log(imageURL);
-    const imageElement = document.createElement('img');
-    imageElement.src = imageURL;
-    document.body.append(imageElement);
-})
-.catch((err)=>{
-    console.log(err);
-}   );
+    .then((response) => response.json())
+    .then(({ data }: GiphyRandomResponse) => {
+        // const imageURL = data.data.images.original.url;
+        const imageURL = data.images.original.url;
+        createImageInsideDOM(imageURL);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
